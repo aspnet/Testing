eb4fbdbefe1872ec43bd70c5c7b2e50846680c85
Cesar Blum Silveira <cesars@microsoft.com>
2015-11-12 17:11:55 -0800

    New attribute to skip tests based on an environment variable.

---
+++ src/Microsoft.AspNet.Testing/xunit/EnvironmentVariableSkipConditionAttribute.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNet.Testing.xunit;
+
+namespace Microsoft.AspNet.Testing.xunit
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class EnvironmentVariableSkipConditionAttribute : Attribute, ITestCondition
+    {
+        private readonly string _environmentVariableName;
+        private readonly string _environmentVariableValue;
+
+        public EnvironmentVariableSkipConditionAttribute(string environmentVariableName, string environmentVariableValue)
+        {
+            _environmentVariableName = environmentVariableName;
+            _environmentVariableValue = environmentVariableValue;
+            SkipReason = $"Test cannot run when environment variable {environmentVariableName} is set to {environmentVariableValue}";
+        }
+
+        public bool IsMet
+        {
+            get
+            {
+                var skip = _environmentVariableValue.Equals(Environment.GetEnvironmentVariable(_environmentVariableName));
+                return !skip;
+            }
+        }
+
+        public string SkipReason { get; set; }
+    }
+}
