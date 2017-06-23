f7f76a939dbc25df1c734ecdc054de1116cd0d51
Cesar Blum Silveira <cesars@microsoft.com>
2017-06-23 15:37:22 -0700

    Restore net46 TFM.

--- test/Microsoft.AspNetCore.Testing.Tests/TestPathUtilitiesTest.cs
+++ test/Microsoft.AspNetCore.Testing.Tests/TestPathUtilitiesTest.cs
@@ -15,6 +15,7 @@ namespace Microsoft.AspNetCore.Testing
             // Directory.GetCurrentDirectory() gives:
             // Testing\test\Microsoft.AspNetCore.Testing.Tests\bin\Debug\netcoreapp2.0
             // Testing\test\Microsoft.AspNetCore.Testing.Tests\bin\Debug\net461
+            // Testing\test\Microsoft.AspNetCore.Testing.Tests\bin\Debug\net46
             var expectedPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", ".."));
 
             Assert.Equal(expectedPath, TestPathUtilities.GetSolutionRootDirectory("Testing"));
